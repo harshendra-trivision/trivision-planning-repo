4978ea29b1d2960b24bd911cea8aff83da567a97
harshendra-trivision <harsh@trivision.ai>
2026-03-18 11:42:52 +0530

    resource.py syntex error fix

--- freppledb/output/views/resource.py
+++ freppledb/output/views/resource.py
@@ -218,9 +218,11 @@ class OverviewReport(GridPivot):
                 f.editable = False
                 reportclass.rows += (f,)
                 if f.formatter == "detail":
-                    reportclass.attr_sql += f"res.{ f.field_name.split("__")[0] }_id, "
+                    attr_name = f.field_name.split("__")[0]
+                    reportclass.attr_sql += f"res.{attr_name}_id, "
                 else:
-                    reportclass.attr_sql += f"res.{ f.field_name.split("__")[-1] }, "
+                    attr_name = f.field_name.split("__")[-1]
+                    reportclass.attr_sql += f"res.{attr_name}, "
             # Adding custom location attributes
             for f in getAttributeFields(
                 Location, related_name_prefix="location", initially_hidden=True
